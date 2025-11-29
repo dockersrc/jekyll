e626b776ce3c9854aec8a7d6379ed2b840c5c687
casjay <git-admin@casjaysdev.pro>
2025-11-29 11:55:56 -0500

    🗃️ Fixed rootfs/** files 🗃️
    Dockerfile
    .env.scripts
    rootfs/.gitea/workflows/docker.yaml
    rootfs/root/docker/setup/00-init.sh
    rootfs/root/docker/setup/01-system.sh
    rootfs/root/docker/setup/02-packages.sh
    rootfs/root/docker/setup/03-files.sh
    rootfs/root/docker/setup/04-users.sh
    rootfs/root/docker/setup/05-custom.sh
    rootfs/root/docker/setup/06-post.sh
    rootfs/root/docker/setup/07-cleanup.sh
    rootfs/usr/local/bin/copy
    rootfs/usr/local/bin/entrypoint.sh
    rootfs/usr/local/bin/symlink
    rootfs/usr/local/etc/docker/functions/entrypoint.sh

--- rootfs/root/docker/setup/05-custom.sh
+++ rootfs/root/docker/setup/05-custom.sh
@@ -1,61 +1,42 @@
 #!/usr/bin/env bash
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-##@Version           :  202511291200-git
+# shellcheck shell=bash
+# - - - - - - - - - - - - - - - - - - - - - - - - -
+##@Version           :  202511291153-git
 # @@Author           :  CasjaysDev
 # @@Contact          :  CasjaysDev <docker-admin@casjaysdev.pro>
 # @@License          :  MIT
-# @@ReadME           :
-# @@Copyright        :  Copyright 2023 CasjaysDev
-# @@Created          :  Mon Aug 28 06:48:42 PM EDT 2023
+# @@Copyright        :  Copyright 2025 CasjaysDev
+# @@Created          :  Sat Nov 29 11:53:11 AM EST 2025
 # @@File             :  05-custom.sh
-# @@Description      :  script to install Jekyll with GitHub Pages support
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# shellcheck shell=bash
-# shellcheck disable=SC2016
-# shellcheck disable=SC2031
-# shellcheck disable=SC2120
-# shellcheck disable=SC2155
-# shellcheck disable=SC2199
-# shellcheck disable=SC2317
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# @@Description      :  script to run custom
+# @@Changelog        :  newScript
+# @@TODO             :  Refactor code
+# @@Other            :  N/A
+# @@Resource         :  N/A
+# @@Terminal App     :  yes
+# @@sudo/root        :  yes
+# @@Template         :  templates/dockerfiles/init_scripts/05-custom.sh
+# - - - - - - - - - - - - - - - - - - - - - - - - -
+# shellcheck disable=SC1001,SC1003,SC2001,SC2003,SC2016,SC2031,SC2090,SC2115,SC2120,SC2155,SC2199,SC2229,SC2317,SC2329
+# - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set bash options
 set -o pipefail
 [ "$DEBUGGER" = "on" ] && echo "Enabling debugging" && set -x$DEBUGGER_OPTIONS
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set env variables
 exitCode=0
-LANG_VERSION="${LANG_VERSION:-latest}"
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - - - - - - -
 # Predefined actions
-echo "Installing Jekyll with GitHub Pages support"
 
-# Install Jekyll and GitHub Pages dependencies
-if [ "$LANG_VERSION" = "latest" ]; then
-  echo "Installing latest Jekyll with github-pages gem..."
-  gem install jekyll bundler github-pages webrick || exitCode=1
-else
-  echo "Installing Jekyll version ${LANG_VERSION}..."
-  gem install jekyll:${LANG_VERSION} bundler github-pages webrick || exitCode=1
-fi
+# - - - - - - - - - - - - - - - - - - - - - - - - -
+# Main script
 
-# Verify installation
-if command -v jekyll >/dev/null 2>&1; then
-  echo "Jekyll installed successfully"
-  jekyll --version || exitCode=1
-  echo "Installed gems:"
-  gem list | grep -E "(jekyll|github-pages|bundler)" || true
-else
-  echo "Jekyll installation failed" >&2
-  exitCode=1
-fi
-
-# Create default site directory
-mkdir -p /srv/jekyll || exitCode=1
-chown -R root:root /srv/jekyll || true
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set the exit code
+#exitCode=$?
+# - - - - - - - - - - - - - - - - - - - - - - - - -
 exit $exitCode
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# - - - - - - - - - - - - - - - - - - - - - - - - -
 # ex: ts=2 sw=2 et filetype=sh
+# - - - - - - - - - - - - - - - - - - - - - - - - -
